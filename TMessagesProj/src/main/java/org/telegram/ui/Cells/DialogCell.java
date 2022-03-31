d14b179698075597164fe258bfa61c62e17a0fd5
blxueya <blxueya@outlook.com>
2022-03-31 17:29:02 +0800

    feat: show Forwarder Name

--- TMessagesProj/src/main/java/org/telegram/ui/Cells/DialogCell.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Cells/DialogCell.java
@@ -1030,7 +1030,7 @@ public class DialogCell extends BaseCell {
                             if (chat != null && chat.id > 0 && fromChat == null && (!ChatObject.isChannel(chat) || ChatObject.isMegagroup(chat))) {
                                 if (message.isOutOwner()) {
                                     messageNameString = LocaleController.getString("FromYou", R.string.FromYou);
-                                } else if (message != null && message.messageOwner.fwd_from != null && message.messageOwner.fwd_from.from_name != null) {
+                                } else if (message != null && message.messageOwner.fwd_from != null && message.messageOwner.fwd_from.from_name != null && !GuGuConfig.ShowForwarderName) {
                                     messageNameString = message.messageOwner.fwd_from.from_name;
                                 } else if (fromUser != null) {
                                     if (useForceThreeLines || SharedConfig.useThreeLinesLayout) {
--- TMessagesProj/src/main/java/tw/nekomimi/nekogram/settings/NekoExperimentalSettingsActivity.java
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/settings/NekoExperimentalSettingsActivity.java
@@ -58,6 +58,7 @@ public class NekoExperimentalSettingsActivity extends BaseNekoSettingsActivity {
 
     private int guguSettingsRow;
     private int alwaysSaveChatOffsetRow;
+    private int showForwarderNameRow;
     private int showSpoilersDirectlyRow;
     private int forceAllowCopyRow;
     private int hideSponsoredMessageRow;
@@ -209,6 +210,11 @@ public class NekoExperimentalSettingsActivity extends BaseNekoSettingsActivity {
             if (view instanceof TextCheckCell) {
                 ((TextCheckCell) view).setChecked(GuGuConfig.HideSponsoredMessage);
             }
+        } else if (position == showForwarderNameRow) {
+            GuGuConfig.toggleShowForwarderName();
+            if (view instanceof TextCheckCell) {
+                ((TextCheckCell) view).setChecked(GuGuConfig.ShowForwarderName);
+            }
         }
     }
 
@@ -299,6 +305,7 @@ public class NekoExperimentalSettingsActivity extends BaseNekoSettingsActivity {
 
         guguSettingsRow = rowCount++;
         alwaysSaveChatOffsetRow = rowCount++;
+        showForwarderNameRow = rowCount++;
         showSpoilersDirectlyRow = rowCount++;
         forceAllowCopyRow = rowCount++;
         hideSponsoredMessageRow = rowCount++;
@@ -351,6 +358,8 @@ public class NekoExperimentalSettingsActivity extends BaseNekoSettingsActivity {
                         textCell.setTextAndCheck(LocaleController.getString("hideSponsoredMessage", R.string.HideSponsoredMessage), GuGuConfig.HideSponsoredMessage,true);
                     } else if (position == showSpoilersDirectlyRow) {
                         textCell.setTextAndCheck(LocaleController.getString("ShowSpoilersDirectly", R.string.ShowSpoilersDirectly), GuGuConfig.ShowSpoilersDirectly,true);
+                    } else if (position == showForwarderNameRow) {
+                        textCell.setTextAndCheck(LocaleController.getString("ShowForwarderName", R.string.ShowForwarderName), GuGuConfig.ShowForwarderName,true);
                     }
                     break;
                 }
