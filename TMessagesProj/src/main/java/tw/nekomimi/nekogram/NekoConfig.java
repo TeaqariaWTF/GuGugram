ab444d018f2e209f407c19a0e8c735634ccf47f1
blxueya <blxueya@outlook.com>
2022-03-31 22:54:39 +0800

    feat: notification

--- TMessagesProj/src/main/java/tw/nekomimi/nekogram/NekoConfig.java
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/NekoConfig.java
@@ -984,8 +984,7 @@ public class NekoConfig {
             }
             return color;
         } else {
-            return 0xff11acfa;
-        }
+            return Color.parseColor("#E4C1DB");        }
     }
 
     public static File getTelegramPath() {
