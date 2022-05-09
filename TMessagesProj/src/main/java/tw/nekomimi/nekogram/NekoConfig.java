6cd8b4c0eabccb35c34d04e9fc63745829096a8e
blxueya <blxueya@outlook.com>
2022-05-09 13:30:14 +0800

    fix:merge upstream

--- TMessagesProj/src/main/java/tw/nekomimi/nekogram/NekoConfig.java
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/NekoConfig.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Build;
 import android.os.Environment;
