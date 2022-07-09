fea7806c4aa027dae89706c58817c17dcb0e68ac
omg-xtao <100690902+omg-xtao@users.noreply.github.com>
2022-07-09 17:23:21 +0800

    fix: dialog do not show channel name

--- TMessagesProj/src/main/java/org/telegram/ui/Cells/DialogCell.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Cells/DialogCell.java
@@ -1024,7 +1024,7 @@ public class DialogCell extends BaseCell {
                                     }
                                 }
                             }
-                            if (chat != null && chat.id > 0 && fromChat == null && (!ChatObject.isChannel(chat) || ChatObject.isMegagroup(chat))) {
+                            if (chat != null && chat.id > 0 && (!ChatObject.isChannel(chat) || ChatObject.isMegagroup(chat))) {
                                 if (message.isOutOwner()) {
                                     messageNameString = LocaleController.getString("FromYou", R.string.FromYou);
                                 } else if (message != null && message.messageOwner.fwd_from != null && message.messageOwner.fwd_from.from_name != null && !GuGuConfig.showForwarderName.Bool()) {
@@ -1039,8 +1039,10 @@ public class DialogCell extends BaseCell {
                                     } else {
                                         messageNameString = UserObject.getFirstName(fromUser).replace("\n", "");
                                     }
+                                } else if (fromChat != null){
+                                    messageNameString = fromChat.title.replace("\n", "");
                                 } else {
-                                    messageNameString = "DELETED";
+                                    messageNameString = LocaleController.getString("HiddenName", R.string.HiddenName);
                                 }
                                 checkMessage = false;
                                 SpannableStringBuilder stringBuilder;
